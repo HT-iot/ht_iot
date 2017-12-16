0f9b89147207a8772cf4d0d4a85a16616631e7c9
Tao Yang <mike.yang@foxmail.com>
2017-12-16 12:36:58 +0800

    change the password in cookie to SHA1 data

--- controllers/login.go
+++ controllers/login.go
@@ -1,8 +1,13 @@
 package controllers
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
 	"fmt"
 	"ht_iot/models"
+	"io"
+
+	"github.com/astaxie/beego/logs"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
@@ -37,18 +42,21 @@ func (c *LoginController) Post() {
 	fmt.Println("q, err =", q, err)
 
 	if len(q) > 0 {
-		fmt.Println("register pwd")
 		maxAge := 0
 		if autoLogin {
 			maxAge = 1<<31 - 1
 		}
 		c.Ctx.SetCookie("uname", uname, maxAge, "/")
+		h := sha1.New()
+		io.WriteString(h, uname+pwd)
+		pwd = base64.StdEncoding.EncodeToString(h.Sum(nil))
 		c.Ctx.SetCookie("pwd", pwd, maxAge, "/")
+		/* TODO add session support
+		c.SetSession(uname, pwd)
+		*/
 		IsLogin = true
 		c.Data["ISLogin"] = IsLogin
-		fmt.Println("IsLogin =", IsLogin)
 
-		//, IsStatus, IsPconfig, IsOconfig
 		c.TplName = "home.html"
 	} else {
 		IsLogin = false
@@ -71,17 +79,33 @@ func checkAccount(ctx *context.Context) bool {
 	}
 	pwd := ck.Value
 
-	p := models.User{Email: uname, Password: pwd}
+	/*
+		TODO add the session support in the furture to reduce query user table
+
+		sid := ctx.Input.CruSession.Get(uname)
+		logs.Debug("get session", sid)
+	*/
+
+	p := models.User{Email: uname, Password: ""}
 	q, err := models.GetAllUers(p)
+	//fmt.Println("q, err =", q, err)
 
-	//	fmt.Println("q, err =", q, err)
-
-	if len(q) > 0 {
-		IsLogin = true
-		return true
-	} else {
+	if len(q) < 1 {
+		logs.Debug("Query user table failure")
 		IsLogin = false
 		return false
-
 	}
+
+	h := sha1.New()
+	io.WriteString(h, q[0].Email+q[0].Password)
+	if pwd == base64.StdEncoding.EncodeToString(h.Sum(nil)) {
+		logs.Debug("compare pwd success")
+		IsLogin = true
+		return true
+	}
+
+	logs.Debug("Cookie password check failure")
+	IsLogin = false
+	return false
+
 }
